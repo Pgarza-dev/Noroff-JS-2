1c236a9c73ff3b32988793a56751e58e88b18e93
Pgarza-dev <84074301+Pgarza-dev@users.noreply.github.com>
2023-09-11 21:20:12 +0100

    updates on all comments

--- js/services/auth.js
+++ js/services/auth.js
@@ -1,8 +1,6 @@
 import { API_BASE_URL } from "../consts.js";
 const registerUrl = `${API_BASE_URL}/social/auth/register`;
 
-// 1. Register User. Create a function that takes in a url and a user object
-
 /**
  * API call to register a user
  * @param {string} url
@@ -10,9 +8,8 @@ const registerUrl = `${API_BASE_URL}/social/auth/register`;
  * ```js
  * registerUser( registerUrl, userToRegister);
  */
-async function registerUser(userData) {
+export async function registerUser(userData) {
   try {
-    // Api call
     const postData = {
       method: "POST",
       headers: {
@@ -22,27 +19,16 @@ async function registerUser(userData) {
     };
     const response = await fetch(registerUrl, postData);
     const json = await response.json();
-    console.log(json);
+    return json;
   } catch (error) {
-    console.log(error + "Something went wrong");
+    console.error(error + "Something went wrong");
   }
 }
 
-const userToRegister = {
-  name: "PabloGarza",
-  email: "pabgar91211@stud.noroff.no",
-  password: "thisismypassword",
-};
-
-registerUser(userToRegister);
-
-// 2.Login the User. Call the function with the url and the user object
-
 const loginUrl = `${API_BASE_URL}/social/auth/login`;
 
-async function loginUser(userData) {
+export async function loginUser(userData) {
   try {
-    // Api call
     const postData = {
       method: "POST",
       headers: {
@@ -52,17 +38,10 @@ async function loginUser(userData) {
     };
     const response = await fetch(loginUrl, postData);
     const json = await response.json();
-    console.log(json);
     const accessToken = json.accessToken;
     localStorage.setItem("accessToken", accessToken);
+    return json;
   } catch (error) {
-    console.log(error + "Something went wrong");
+    console.error(error + "Something went wrong");
   }
 }
-
-const userToLogin = {
-  email: "pabgar91211@stud.noroff.no",
-  password: "thisismypassword",
-};
-
-loginUser(userToLogin);
