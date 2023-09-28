8d198045f274b128797fac3f8e8425b0b1857a16
Pablo Garza <84074301+Pgarza-dev@users.noreply.github.com>
2023-09-28 14:12:29 +0100

    edit profile function/get username from querrypram (#44)
    edit UI.

--- src/lib/page_scripts/profile.js
+++ src/lib/page_scripts/profile.js
@@ -5,6 +5,8 @@ import { getAllPosts } from "../services/posts.js";
 import { createFormDataObject } from "@lib/forms/utils";
 import { getProfilePosts } from "../services/posts.js";
 import { Post } from "../../components/post/post.js";
+import { editProfile } from "../utils/editProfile.js";
+import { getUsernameQueryParam } from "../utils/getUsernameQueryParam.js";
 
 const myPostUrl = `${API_BASE_URL}/social/posts?_author=true&_comments=true&_reactions=true`;
 const createPostButton = document.getElementById("create-post-button");
@@ -12,18 +14,25 @@ const newPostForm = document.getElementById("new-post-form");
 const newPostDiv = document.getElementById("new-post");
 const createPostPlusIcon = document.getElementById("create-post-plus-icon");
 const createPostMinusIcon = document.getElementById("create-post-minus-icon");
+const cancelPostButton = document.getElementById("cancel-post-button");
 
 createPostButton.addEventListener("click", displayInput);
+
+createPostButton.addEventListener("click", () => {
+  createPostButton.classList.add("theCard");
+  createPostButton.classList.add("flip");
+});
+
 newPostForm.addEventListener("submit", createNewPost);
+
+editProfile();
 
 async function createNewPost(event) {
   event.preventDefault();
   const form = createFormDataObject(newPostForm);
   try {
     const response = await createPost(form);
-    console.log("Post created:", response);
 
-    console.log(response);
     const postDiv = document.createElement("div");
     postDiv.classList.add("post");
     postDiv.innerHTML = `<p>${response.title}</p><p>${response.body}</p>`;
@@ -31,11 +40,6 @@ async function createNewPost(event) {
   } catch (error) {
     console.error(error);
   }
-}
-
-async function displayAllPosts() {
-  const profilePostsSection = document.getElementById("profile-posts");
-  console.log(posts);
 }
 
 function displayInput() {
@@ -46,9 +50,8 @@ function displayInput() {
 
 const profilePostsSection = document.getElementById("profile-posts");
 async function main() {
-  const posts = await getProfilePosts("dfd");
-
-  console.log(posts);
+  const username = getUsernameQueryParam();
+  const posts = await getProfilePosts(username);
 
   posts.forEach((post) => {
     profilePostsSection.appendChild(new Post(post));
@@ -56,11 +59,3 @@ async function main() {
 }
 
 main();
-
-// add event listener to input post form
-//add event listener to new post button.
-//after button click, display input fields for new post
-//after input fields are filled out, add event listener to submit button
-//after submit button is clicked, send post request to api
-//after post request is sent, display new post on page
-//{{baseUrl}}/social/posts?_author=true&_comments=true&_reactions=true
