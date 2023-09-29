c6baccbe691d898d905c0a457e876021de11b8f8
Pgarza-dev <84074301+Pgarza-dev@users.noreply.github.com>
2023-09-29 13:48:33 +0100

    profile service

---
+++ src/lib/services/profiles.js
@@ -0,0 +1,45 @@
+import { fetcher } from "./fetcher";
+import { API_BASE_URL } from "../constants.js";
+
+export async function getAllProfiles() {
+  return await fetcher({
+    url: `${API_BASE_URL}/social/profiles`,
+  });
+}
+
+export async function getSingleProfile(username) {
+  return await fetcher({
+    url: `${API_BASE_URL}/social/profiles/${username}`,
+  });
+}
+
+export async function getSingleProfilePosts(username) {
+  return await fetcher({
+    url: `${API_BASE_URL}/social/profiles/${username}/posts`,
+  });
+}
+
+export async function updateEntryMedia(username, avatarUrl, bannerUrl) {
+  return await fetcher({
+    url: `${API_BASE_URL}/social/profiles/${username}/media`,
+    method: "PUT",
+    body: {
+      avatar: avatarUrl,
+      banner: bannerUrl,
+    },
+  });
+}
+
+export async function updateEntryProfile(username) {
+  return await fetcher({
+    url: `${API_BASE_URL}/social/profiles/${username}/profile`,
+    method: "PUT",
+  });
+}
+
+export async function followProfile(username) {
+  return await fetcher({
+    url: `${API_BASE_URL}/social/profiles/${username}/follow`,
+    method: "PUT",
+  });
+}
