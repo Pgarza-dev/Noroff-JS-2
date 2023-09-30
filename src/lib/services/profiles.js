4e6964713f3c26fd29b296e27cfb5f9715af08ce
Pablo Garza <84074301+Pgarza-dev@users.noreply.github.com>
2023-09-30 11:21:03 +0100

    profile service (#46)
    * profile service
    
    * Add follow/unfollow Api functions
    
    * split entryMedia function to profile and banner

---
+++ src/lib/services/profiles.js
@@ -0,0 +1,53 @@
+import { fetcher } from "./fetcher";
+import { API_BASE_URL } from "../constants.js";
+
+export async function getAllProfiles() {
+  return await fetcher({
+    url: `${API_BASE_URL}/social/profiles`,
+  });
+}
+
+export async function getSingleProfile(username) {
+  return await fetcher({
+    url: `${API_BASE_URL}/social/profiles/${username}`,
+  });
+}
+
+export async function getSingleProfilePosts(username) {
+  return await fetcher({
+    url: `${API_BASE_URL}/social/profiles/${username}/posts`,
+  });
+}
+
+export async function updateProfilePicture(username, avatarUrl) {
+  return await fetcher({
+    url: `${API_BASE_URL}/social/profiles/${username}/media`,
+    method: "PUT",
+    body: {
+      avatar: avatarUrl,
+    },
+  });
+}
+export async function updateProfileBanner(username, bannerUrl) {
+  return await fetcher({
+    url: `${API_BASE_URL}/social/profiles/${username}/media`,
+    method: "PUT",
+    body: {
+      banner: bannerUrl,
+    },
+  });
+}
+
+export async function followProfile(username) {
+  return await fetcher({
+    url: `${API_BASE_URL}/social/profiles/${username}/follow`,
+    method: "PUT",
+  });
+}
+
+export async function unFollowProfile(username) {
+  return await fetcher({
+    url: `${API_BASE_URL}/social/profiles/${username}/unfollow`,
+    method: "PUT",
+  });
+}
