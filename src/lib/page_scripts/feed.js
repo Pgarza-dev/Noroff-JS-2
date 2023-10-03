234892b6fc083793f44e78098c74e142c2abd5fe
Pablo Garza <84074301+Pgarza-dev@users.noreply.github.com>
2023-10-03 19:45:48 +0100

    Post feed input (#63)
    * added feed page post button
    
    * merge dev
    
    * updated posts on feed page
    
    ---------
    
    Co-authored-by: Kyrre Gjerstad <kyrregjerstad@gmail.com>

--- src/lib/page_scripts/feed.js
+++ src/lib/page_scripts/feed.js
@@ -1,6 +1,10 @@
-import { getAllPosts } from "../services/posts.js";
 import { Post } from "@components/post/post.js";
+import { createFormDataObject } from "@lib/forms/utils";
+import { createPost, getAllPosts } from "../services/posts.js";
 import { postStore } from "../stores/postStore.js";
+
+const postInput = document.getElementById("post-input");
+const postsSection = document.querySelector("#posts");
 
 async function initPostStore() {
   const postsData = await getAllPosts();
@@ -14,12 +18,25 @@ function subscribeToPostStore(renderFeed) {
 }
 
 function renderFeed(postsData) {
-  const postsSection = document.querySelector("#posts");
   postsSection.innerHTML = "";
 
   postsData.forEach((postData) => {
     postsSection.appendChild(new Post(postData));
   });
+}
+
+postInput.addEventListener("submit", createNewFeedPost);
+
+async function createNewFeedPost(event) {
+  event.preventDefault();
+  const form = createFormDataObject(event.target);
+
+  const response = await createPost(form);
+
+  postStore.setState((state) => ({
+    posts: [response, ...state.posts],
+  }));
+  postInput.hidePopover();
 }
 
 async function initPage() {
