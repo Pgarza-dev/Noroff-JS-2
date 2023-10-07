a38ef6f52a4012b4c091513988080b8766b87904
Pgarza-dev <84074301+Pgarza-dev@users.noreply.github.com>
2023-10-07 20:38:42 +0100

    add filter btn + functionality for follower post

--- src/lib/page_scripts/feed.js
+++ src/lib/page_scripts/feed.js
@@ -1,14 +1,23 @@
+console.log("feed.js loaded");
+
 import { Post } from "@components/post/post.js";
 import { createFormDataObject } from "@lib/forms/utils";
 import { createPost, getAllPosts } from "../services/posts.js";
 import { postStore } from "../stores/postStore.js";
+import { fetcher } from "../services/fetcher.js";
+import { getFollowersPosts } from "../services/posts.js";
+import { getUsernameQueryParam } from "../utils/getUsernameQueryParam.js";
 
 const postInput = document.getElementById("post-input");
-const postsSection = document.querySelector("#posts");
+const postsSection = document.getElementById("posts");
+const filterPostSelect = document.getElementById("filter-posts-select");
+const oldestFeed = document.getElementById("oldest-posts-feed");
 
 async function initPostStore() {
   const postsData = await getAllPosts();
   postStore.setState(() => ({ posts: postsData }));
+
+  console.log("postStore", postStore.getState());
 }
 
 function subscribeToPostStore(renderFeed) {
@@ -49,3 +58,21 @@ async function initPage() {
 }
 
 initPage();
+
+filterPostSelect.addEventListener("change", filterPostsHandler);
+
+function filterPostsHandler(event) {
+  console.log(event.target.value);
+  if (event.target.value === "following") {
+    friendsOnlyFeedHandler();
+    console.log("filterPostsHandler", event.target.value);
+  }
+}
+
+async function friendsOnlyFeedHandler() {
+  const username = getUsernameQueryParam();
+  const postsData = await getFollowersPosts(username);
+  console.log(postsData);
+
+  renderFeed(postsData);
+}
