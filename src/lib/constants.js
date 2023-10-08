780d1b90b573fde6d99e219c9da780f31834f22e
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2023-10-08 16:36:18 +0200

    Fix/add auth free paths (#83)
    * add better unauthorized error handling
    
    * fix trailing slash in header
    
    * fix settings build
    
    * fix loging errors

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -3,7 +3,14 @@ export const API_BASE_URL = "https://api.noroff.dev/api/v1";
 export const USERNAME_MIN_LENGTH = 1;
 export const PASSWORD_MIN_LENGTH = 8;
 export const EMAIL_DOMAIN_WHITELIST = ["@noroff.no", "@stud.noroff.no"];
-export const AUTH_FREE_PATHS = ["/login/", "/signup/", "/unauthorized/"];
+export const AUTH_FREE_PATHS = [
+  "/login/",
+  "/login",
+  "/signup/",
+  "/signup",
+  "/unauthorized/",
+  "/unauthorized",
+];
 
 export const ALL_POSTS_ENDPOINT = "/social/posts";
 export const SINGLE_POST_ENDPOINT = "/social/posts/";
