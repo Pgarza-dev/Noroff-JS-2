7b2f631fa67ee5fd81587c69c93016ec35d69cfa
Pablo Garza <84074301+Pgarza-dev@users.noreply.github.com>
2023-10-08 10:35:18 +0100

    Filter posts (#74)
    * update cancel post  button UI on feed page
    
    * update post title input UI to match post body
    
    * update post media input UI to match body & title
    
    * added border/shadow to feed post modal
    
    * remove shadow from feed page post modal
    
    * updated login & register <a> tags UI
    
    * updated user.js for posting with new UI
    
    * removed commented code from user.js / index.html
    
    * added suggested changes to UI
    
    * add filter btn + functionality for follower post
    
    * add all-posts filter option + functionality
    
    * post filter btn styled
    
    * fix alignment
    
    ---------
    
    Co-authored-by: Kyrre Gjerstad <kyrregjerstad@gmail.com>

--- src/lib/page_scripts/feed.js
+++ src/lib/page_scripts/feed.js
@@ -1,14 +1,22 @@
+console.log("feed.js loaded");
+
 import { Post } from "@components/post/post.js";
 import { createFormDataObject } from "@lib/forms/utils";
 import { createPost, getAllPosts } from "../services/posts.js";
 import { postStore } from "../stores/postStore.js";
+import { getFollowersPosts } from "../services/posts.js";
+import { getUsernameQueryParam } from "../utils/getUsernameQueryParam.js";
 
 const postInput = document.getElementById("post-input");
-const postsSection = document.querySelector("#posts");
+const postsSection = document.getElementById("posts");
+const filterPostSelect = document.getElementById("filter-posts-select");
+const allPosts = document.getElementById("oldest-posts-feed");
 
 async function initPostStore() {
   const postsData = await getAllPosts();
   postStore.setState(() => ({ posts: postsData }));
+
+  console.log("postStore", postStore.getState());
 }
 
 function subscribeToPostStore(renderFeed) {
@@ -46,3 +54,24 @@ async function initPage() {
 }
 
 initPage();
+
+filterPostSelect.addEventListener("change", filterPostsHandler);
+
+function filterPostsHandler(event) {
+  console.log(event.target.value);
+  if (event.target.value === "following") {
+    friendsOnlyFeedHandler();
+    console.log("filterPostsHandler", event.target.value);
+  } else if (event.target.value === "all-posts") {
+    renderFeed(postStore.getState((state) => state.posts));
+    console.log("filterPostsHandler", event.target.value);
+  }
+}
+
+async function friendsOnlyFeedHandler() {
+  const username = getUsernameQueryParam();
+  const postsData = await getFollowersPosts(username);
+  console.log(postsData);
+
+  renderFeed(postsData);
+}
